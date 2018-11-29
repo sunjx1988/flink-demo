000c6cd4be3e9feced0a8bd5955b38983b733fbc
sunjx <sunjx1988@163.com>
2018-11-29 18:03:10 +0800

    BUG 消息格式问题导致任务不能继续

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 [flink 消费 kafka消息 输出到redis](https://www.cnblogs.com/jiashengmei/p/9084057.html)
 
 [Flume+Kafka+Flink+Redis构建大数据实时处理系统：实时统计网站PV、UV展示](https://yq.aliyun.com/articles/635327)
+
+
+> demo 1
+
+```
+sunjx.demo.kafka.KafkaDemo
+
+--input-topic 'test' --output-topic 'test_out' --bootstrap.servers 'localhost:9092' --zookeeper.connect 'localhost:2181' --group.id 'test'
+```
--- demo-1.0/src/main/java/sunjx/demo/kafka/KafkaProducerTest.java
+++ demo-1.0/src/main/java/sunjx/demo/kafka/KafkaProducerTest.java
@@ -25,12 +25,14 @@ public class KafkaProducerTest {
         Producer<String, String> producer = new KafkaProducer<>(props);
         int totalMessageCount = 10000;
         for (int i = 0; i < totalMessageCount; i++) {
-            String value = String.format("%d,%s,%d", System.currentTimeMillis(), "machine-1", currentMemSize());
+            String value = String.format("%s,%d,%d","machine-1", currentMemSize(), System.currentTimeMillis());
             producer.send(new ProducerRecord<>("test", value), new Callback() {
                 @Override
                 public void onCompletion(RecordMetadata metadata, Exception exception) {
                     if (exception != null) {
                         System.out.println("Failed to send message with exception " + exception);
+                    }else{
+                        System.out.println("send success");
                     }
                 }
             });
